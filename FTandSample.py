9fb3d7058ef7b3f4913efa3949d421ecdc862b32
lizzie-lepton <elizabeth.campbell11@imperial.ac.uk>
2014-07-24 15:47:07 +0100

    Add FTandSample.py

--- FTandSample.py
+++ FTandSample.py
@@ -17,16 +17,27 @@ import scipy.misc as misc
 
 
 
-def create_disc(j, k, a, b, r, n):
-    array = np.zeros((j,k))
+def create_disc(j, k, l, a, b, r, n):
+    array = np.zeros((j,k,l))
     y,x = np.ogrid[-a:n-a, -b:n-b]
     mask = x*x + y*y <= r*r
     array[mask]= 1
     return array
 
-#def make_antennas():
+def make_antennas(n,m,o, p, q):
+    array = np.zeros((n,m,o))
+    y,x = np.ogrid[0:n, 0:n]
+    mask1 = y == p
+    mask2 = x==q
+    array[mask1]= 1
+    array[mask2] = 1
+    return array
+    
 
-#def input_signal():
+def image_input(filename):
+    img = mpimg.imread(filename)
+    return img
+
     
 
 def make_image(array):
@@ -55,7 +66,7 @@ def sample(array1,array2):
     return np.multiply(array1,array2)
 
 def continuous_uv_plane(array1, array2):
-    uv = sig.convolve2d(array1, array2, "same")
+    uv = sig.convolve(array1, array2, "same")
     return uv
 
 def dirty_beam(array):
@@ -69,41 +80,62 @@ def discrete_uv_plane(array1, array2):
     b =np.zeros(n)
 
     for element in xrange(n):
+        x1,y1 = np.ogrid[0:n,0:n]
         if array1[element] == 0:
             b[element] = 0
             
         elif array1[element] == 1:
+            x2,y2 = np.ogrid[0:n,0:n]
             for i in xrange(n):
                 if array2[i] ==1:
-                    #how to define "point"?
-                    # append the new array with point = (x1-x2/0.21, y1-y2/0.21)
+                    x2 = i
+                    point = (x1[element]-x2[i])/redshifted_lambda(1), (y1[element]-y2[i])/redshifted_lambda(1)
                     b[point] = 1
-                
-         
                 elif i ==0:
                     b[element] = 0
 
-def basic_interferometer():
 
-    #take input from wherever
+
+def small_interferometer(): #<100 antennae
+    galaxy = create_disc(8,8,4,5,2,15)
     
-    galaxy = create_disc(8,8,4,5,2,15) #basic example
+    telescope = make_antennas
     
-    ftgal = twod_FT(galaxy) # or input
+    ftgal = twod_FT(galaxy)
+    
+    uvplane = discrete_uv_plane(telescope, telescope)
+    
+    sampled_sky = sample(ftgal, uvplane)
+    
+    dirty_image = inv_twod_ft(sampled_sky)
 
-    #make_antennas
+    dirty_image_view = make_image(dirty_image)
 
-    uvplane = continuous_uv_plane(VLA, VLA) # take output of make_antennas
+    #clean image
 
-    #uvplane = discrete_uv_plane(VLA,VLA)
+
+def large_interferometer(filename): #large >100 antennae
+
+    galaxy = make_image(filename)
+    
+    ftgal = twod_FT(galaxy) 
+
+    telescope = make_antennas
+
+    uvplane = continuous_uv_plane(telescope, telescope) # take output of make_antennas
 
     sampled_sky = sample(ftgal, uvplane)
 
     dirty_image = inv_twod_ft(sampled_sky)
 
-    #dirty_image_view = make_image(dirty_image)
+    dirty_image_view = make_image(dirty_image)
 
     #clean_image = deconvolve(dirty_image)
+
+def redshifted_lambda(z):
+    lambda_obs = (1+z)* 0.21
+
+    return lambda_obs
 
     
 #def deconvolve(array):
